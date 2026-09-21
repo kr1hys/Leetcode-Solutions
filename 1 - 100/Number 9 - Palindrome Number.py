51c3e7492981e0cc82a2fcc7b77fd5c2e9a1e1ad
kr1hys <k13hayes@outlook.com>
2026-09-21 19:27:21 +0100

    Number 9 Completed.

---
+++ 1 - 100/Number 9 - Palindrome Number.py
@@ -0,0 +1,8 @@
+#Number 9 - Palindrome Number
+class Solution(object):
+    def isPalindrome(self, x):
+        return str(x) == str(x)[::-1]
+
+print(Solution().isPalindrome(121))
+print(Solution().isPalindrome(-121))
+print(Solution().isPalindrome(10))
