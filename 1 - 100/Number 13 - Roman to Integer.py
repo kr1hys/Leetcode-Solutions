3ebc885f1ef3093f61ed6f79209cb69b4f40e662
kr1hys <k13hayes@outlook.com>
2026-09-25 11:54:10 +0100

    Removed comments.
    Comments were left from a previous plan which overcomplicated it.

--- 1 - 100/Number 13 - Roman to Integer.py
+++ 1 - 100/Number 13 - Roman to Integer.py
@@ -3,8 +3,6 @@ class Solution(object):
     def romanToInt(self, s):
         romanint = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
         total = 0 
-        #flip it round
-        #add every number together unless its a smaller value from the dic after a bigger number
         for i in range(len(s)):
             if i+1 == len(s):
                 total += romanint[s[i]]
